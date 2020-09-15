48fb7d6dd36e590ad6c3df9cd0e5b2579a95e484
Braian Coronel <braian@itdevexpert.com>
2020-09-14 23:56:26 -0300

    Add consumer to application in domain

--- domain/src/main/kotlin/app/moebius/domain/entity/Application.kt
+++ domain/src/main/kotlin/app/moebius/domain/entity/Application.kt
@@ -9,9 +9,24 @@ import javax.persistence.Table
 data class Application(
         val applicationUUID: UUID,
         val environment: Environment,
+        val consumer: Consumer,
         val hashPassword: String
 )
 
 enum class Environment {
     DEV, INTEGRATION, TESTING, STAGING, PRODUCTION
+}
+
+sealed class Consumer {
+    data class Users(val usersUUID: UUID)
+
+    /**
+     * A partner with a certain goal
+     */
+    data class Partner(val partnerUUID: UUID, val name: String, val goal: String)
+
+    /**
+     * A team consumes a particular feature
+     */
+    data class Team(val teamUUID: UUID, val name: String, val feature: String)
 }
