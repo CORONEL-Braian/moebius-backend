00463d619bf81d2842888757d8981248c640154d
Braian Coronel <braian@itdevexpert.com>
2020-09-17 05:20:03 -0300

    Update consumer in domain

--- domain/src/main/kotlin/app/moebius/domain/entity/Application.kt
+++ domain/src/main/kotlin/app/moebius/domain/entity/Application.kt
@@ -4,29 +4,50 @@ import java.util.*
 import javax.persistence.Entity
 import javax.persistence.Table
 
+/**
+ * Map with:
+ *  . AppConsumerUsers
+ *  . AppConsumerPartner
+ *  . AppConsumerOrganization
+ */
 @Entity
 @Table(name = "application")
 data class Application(
         val applicationUUID: UUID,
         val environment: Environment,
         val consumer: Consumer,
-        val hashPassword: String
+        val publicKey: String
 )
 
 enum class Environment {
     DEV, INTEGRATION, TESTING, STAGING, PRODUCTION
 }
 
+
 sealed class Consumer {
-    data class Users(val usersUUID: UUID)
+    @Entity
+    @Table(name = "consumer_identities")
+    data class Identities(val usersUUID: UUID, val platform: Platform)
 
     /**
-     * A partner with a certain goal
+     * A partner consumes a particular feature
      */
-    data class Partner(val partnerUUID: UUID, val name: String, val goal: String)
+    @Entity
+    @Table(name = "consumer_partner")
+    data class Partner(val partnerUUID: UUID, val name: String, val platform: Platform, val feature: String)
 
     /**
      * A team consumes a particular feature
      */
-    data class Team(val teamUUID: UUID, val name: String, val feature: String)
+    @Entity
+    @Table(name = "consumer_team")
+    data class Team(val teamUUID: UUID, val name: String, val platform: Platform, val feature: String)
 }
+
+@Entity
+@Table(name = "platform")
+data class Platform(
+        val platformUUID: UUID,
+        val name: String,
+        val ecosystem: String
+)
