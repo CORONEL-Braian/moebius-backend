db836333b81ea95d6a6c66a9cc1c010b1b9586fd
Gaston Perez <gaston@itdevexpert.com>
2020-12-13 17:21:10 -0300

    init content model

--- domain/src/main/kotlin/app/mobius/domain/model/Content.kt
+++ domain/src/main/kotlin/app/mobius/domain/model/Content.kt
@@ -1,3 +1,22 @@
-package app.mobius.domain.model
-
-class Content
+import java.util.*
+data class Content(
+        val uuid: UUID,
+        val title: String,
+        val color: Color,
+        val coverPage: String,
+        val description: String,
+        val tag: String,
+        val createOn: Date,
+        val attachments: Attachments,
+)
+typealias Attachments = List<Attached>
+data class Attached(
+        val uuid: UUID,
+        val title: String,
+        val createOn: Date,
+        val file: String,
+)
+data class Color(
+        val title: String,
+        val hexCode: String,
+)
