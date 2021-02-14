90399fd70f03c279544c59886f77314c3a6c9c44
Braian Coronel <braian@itdevexpert.com>
2021-02-13 22:04:38 -0300

    MOBIUS-136 | Add cases of wrong credentials and headers

--- security-core/src/test/kotlin/app/mobius/security/authentication/configuration/SecurityBasicAuthConfigurationIntegrationTest.kt
+++ security-core/src/test/kotlin/app/mobius/security/authentication/configuration/SecurityBasicAuthConfigurationIntegrationTest.kt
@@ -3,14 +3,12 @@ package app.mobius.security.authentication.configuration
 import app.mobius.MobiusFeatureIntegrationTest
 import app.mobius.api.ApiEndpoints.URL_BASE
 import app.mobius.security.SecurityCoreEndpoints
-import app.mobius.security.authentication.controller.SecurityBasicAuthControllerTest
 import app.mobius.security.authentication.controller.SecurityCoreEndpointsTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
@@ -36,20 +34,6 @@ class SecurityBasicAuthConfigurationIntegrationTest {
         ).andExpect(MockMvcResultMatchers.status().isOk)
     }
 
-//    PRECONDITION: App is running because it is a live test
-    /*@Test
-    @Throws(IllegalStateException::class, IOException::class)
-    fun `when someone requests a home endpoint and app is running, then success`() {
-        val fullUrl =  provideFullUrl(SecurityCoreEndpoints.Keys.HOME)
-        val testRestTemplate = TestRestTemplate()
-
-        val response: ResponseEntity<String> = testRestTemplate.getForEntity(
-                fullUrl, String::class
-        )
-
-        Assertions.assertEquals(HttpStatus.OK, response.statusCode)
-    }*/
-
     /**
      * OBS: if you forget to provide credentials (basic auth), your custom auth provider won't be called.
      * Source:
@@ -69,11 +53,43 @@ class SecurityBasicAuthConfigurationIntegrationTest {
     }
 
     @Test
-    fun `when developer with wrong credentials and headers then unauthorized response`() {
-//        "user", "wrongpassword"
-//          "wrong header"
-//        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
-//        assertTrue(response.getBody().contains("Unauthorized"));
+    fun `when developer with wrong password and headers then unauthorized response`() {
+        val requestBuilder = MockMvcRequestBuilders
+                .get(provideFullUrl(SecurityCoreEndpointsTest.Keys.SECURE))
+                .with(httpBasic("braian.coronel", "wrongpassword"))
+                .header("Platform-Name", "Android")
+                .header("Platform-Ecosystem", "Mobile")
+
+        val resultMatcher = MockMvcResultMatchers.status().isUnauthorized
+
+        mockMvc.perform(requestBuilder).andExpect(resultMatcher)
+    }
+
+    @Test
+    fun `when developer with the absence of header platform-name then an exception is throwed`() {
+        val requestBuilder = MockMvcRequestBuilders
+                .get(provideFullUrl(SecurityCoreEndpointsTest.Keys.SECURE))
+                .with(httpBasic("braian.coronel", "145"))
+                .header("Platform-Ecosystem", "Mobile")
+
+        val resultMatcher = MockMvcResultMatchers.status().isOk
+
+        assertThrows<Exception> {
+            mockMvc.perform(requestBuilder).andExpect(resultMatcher)
+        }
+    }
+    @Test
+    fun `when developer with the absence of header platform-ecosystem then an exception is throwed`() {
+        val requestBuilder = MockMvcRequestBuilders
+                .get(provideFullUrl(SecurityCoreEndpointsTest.Keys.SECURE))
+                .with(httpBasic("braian.coronel", "145"))
+                .header("Platform-Name", "Android")
+
+        val resultMatcher = MockMvcResultMatchers.status().isOk
+
+        assertThrows<Exception> {
+            mockMvc.perform(requestBuilder).andExpect(resultMatcher)
+        }
     }
 
 }
