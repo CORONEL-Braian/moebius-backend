a955995925f2c6f898628bc9fab98dee83fb39ce
Braian Coronel <braian@itdevexpert.com>
2021-04-08 00:41:50 -0300

    MapperJsonApi | Add credentials using TestPropertySource in AppAuthServiceTest

--- documentation/Spring.md
+++ documentation/Spring.md
@@ -30,47 +30,13 @@ ___
 ### @QueryParams
 
  * https://stackoverflow.com/a/28993810/5279996
+ 
+---
+### @TestPropertySource
+
+ * https://www.baeldung.com/spring-test-property-source
 
 ___
 ### Headers
 
 [Read HTTP Headers](https://www.baeldung.com/spring-rest-http-headers)
-
-
-
-
----
----
-
-## ERRORS:
-
-`alter table if exists role add column securityLevel int4 not null;`
-[23502] ERROR: column "securitylevel" contains null values
-
-Solution: Add annotation
- * From: `val securityLevel: Int = 0,`
- * To: `@Column(name = "security_level") val securityLevel: Int = 0,`
-
----
-
-Error executing DDL "alter table if exists daily_reload_token add column keepSessionDaily boolean"
- * From: `@Column(name = "keepSessionDaily") val keepSessionDaily: Boolean = false,`
- * To: `@Column(name = "keep_session_daily") val keepSessionDaily: Boolean = false,`
- 
----
-
-When select * with @Query
-
-    @Query(value = "SELECT * FROM platform", nativeQuery = true)
-    fun findAllPlatforms(): List<Any>
-
-Error: `No Dialect mapping for JDBC type: 1111`
-
-Solution:  `CAST(a_uuid as varchar) a_uuid` in query
-
-Source: https://stackoverflow.com/a/54017193/5279996
-
-___
-
-Error: When an Enum is used in JpaRepository nativeQuery
-Solution: https://stackoverflow.com/a/66979684/5279996
--- feature_credential_managment/src/test/kotlin/app/mobius/crendentialManagment/data/service/AppAuthorizationServiceTest.kt
+++ feature_credential_managment/src/test/kotlin/app/mobius/crendentialManagment/data/service/AppAuthorizationServiceTest.kt
@@ -4,12 +4,17 @@ import app.mobius.MobiusFeatureIntegrationTest
 import app.mobius.credentialManagment.data.repository.AppAuthorizationJpaRepository
 import app.mobius.credentialManagment.domain.entity.security.Environment
 import app.mobius.credentialManagment.domain.entity.security.Platform
+import app.mobius.crendentialManagment.data.CredentialManagmentTestPropertySource
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
 import java.util.*
+import javax.annotation.PostConstruct
 
 /**
  * OBS: Not mock JpaRepository, you must use MobiusFeatureIntegrationTest and @Autowired
@@ -17,19 +22,39 @@ import java.util.*
  */
 @AutoConfigureMockMvc
 @SpringBootTest(classes = [MobiusFeatureIntegrationTest::class])
+@ContextConfiguration(classes = [CredentialManagmentTestPropertySource::class])
+@TestPropertySource(locations = ["/testPropertySource.properties"])
 open class AppAuthorizationServiceTest {
 
     @Autowired
     private lateinit var appAuthorizationJpaRepository: AppAuthorizationJpaRepository
 
+    @Autowired
+    private lateinit var propertySource: CredentialManagmentTestPropertySource
+
+    private lateinit var developerName: String
+    private lateinit var privateKey: String
+
+    /**
+     * This scope is executed after autowired for initialize providers of TestPropertySource
+     *
+     * Source:
+     *  . Autowiring order: https://stackoverflow.com/a/38068933/5279996
+     */
+    @PostConstruct
+    fun afterAutowired() {
+        developerName = propertySource.developerName!!
+        privateKey = propertySource.privateKey!!
+    }
+
+    private fun provideDeveloperName(developerName: String = this.developerName) = developerName
+
+    private fun providePrivateKey(privateKey: String = this.privateKey) = privateKey
+
     private fun providePlatform(
             name: String = "Android",
             ecosystem: String = "Mobile"
     ) = Platform(name = name, ecosystem = ecosystem)
-
-    private fun provideDeveloperName(developerName: String = "userForTest") = developerName
-
-    private fun providePrivateKey(privateKey: String = "123") = privateKey
 
     private fun provideEnvironment(environment: Environment = Environment.DEV) = environment
 
@@ -40,6 +65,12 @@ open class AppAuthorizationServiceTest {
     ) = UUID.fromString(
             appAuthorizationJpaRepository.findAppAuthorizationDeveloperUUID(platform, developerName, environment)
     )
+
+    @Test
+    open fun `Given a Test Property Source, When variables retrieved, Then the values are returned`() {
+        Assertions.assertNotNull(propertySource.developerName)
+        Assertions.assertNotNull(propertySource.privateKey)
+    }
 
     @Test
     fun `test a simple routine`() {
